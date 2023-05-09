a7368bf832d97ff6fc2bc91ec09440529ff824e7
Manuk77 <manmovsesyan1985@gmail.com>
2023-05-09 18:45:03 +0400

    new class for creating new instaces for pojo clases

--- src/main/java/airport_hibernate/service/create_pojo_objects/CreateObjects.java
+++ src/main/java/airport_hibernate/service/create_pojo_objects/CreateObjects.java
@@ -1,9 +1,13 @@
 package airport_hibernate.service.create_pojo_objects;
 
+import airport_hibernate.pojo_classes.Address;
 import airport_hibernate.pojo_classes.Company;
 import airport_hibernate.pojo_classes.Passenger;
+import airport_hibernate.pojo_classes.Trip;
+import airport_hibernate.validation.Validation;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.Scanner;
 
 public class CreateObjects {
@@ -17,11 +21,11 @@ public class CreateObjects {
         return objects;
     }
 
-    private Company createObjComp(){
+    public Company createObjComp(){
         Scanner scStr = new Scanner(System.in);
         System.out.print("ENTER COMPANY NAME -> ");
         String name = scStr.next();
-        String regex = "\\d{4}-{2}-{2}";
+        String regex = "\\d{4}-\\d{2}-\\d{2}";
         System.out.println("ENTER FOUNDING DATE YYYY-MM-DD");
         String foundDate = scStr.next();
         while (!foundDate.matches(regex)) {
@@ -34,11 +38,31 @@ public class CreateObjects {
         return company;
     }
 
-    private Passenger createObjPass() {
+    public Passenger createObjPass() {
         Scanner scStr = new Scanner(System.in);
         System.out.print("ENTER THE PASSENGERS NAME -> ");
         String name = scStr.next();
+        String phoneNumber = Validation.getInstance().validPhoneNumber();
+        Address address = Validation.getInstance().validAddress();
 
+        return new Passenger(name, phoneNumber, address);
+    }
 
+    public Trip createObjTrip() {
+        final long tripId = Validation.getInstance().getValidTripId();
+        final Company company = createObjComp();
+        final String townFrom = getTown();
+        final String townTo   = getTown();
+        final Timestamp timeOut = Validation.getInstance().validTimeStamp();
+        final Timestamp timeIn = Validation.getInstance().validTimeStamp();
+        return new Trip(tripId, company, townFrom, townTo, timeOut, timeIn);
+
+    }
+
+    public String getTown() {
+        Scanner scStr = new Scanner(System.in);
+        System.out.print("ENTER THE CITY -> ");
+        String town = scStr.next();
+        return town;
     }
 }
